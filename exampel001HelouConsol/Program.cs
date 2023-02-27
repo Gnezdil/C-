9efd2fd3624b06950e6c4304f8849ac67f44626c
DGnezdilOFF <120dimos@mail.ru>
2023-02-27 20:37:54 +0300

    Exampel 001 Helou Word

--- exampel001HelouConsol/Program.cs
+++ exampel001HelouConsol/Program.cs
@@ -1,5 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Helou world 11!");
-Console.WriteLine("Helou world 11!");
+﻿// привет мир.
 Console.WriteLine("Helou world 11!");
 
